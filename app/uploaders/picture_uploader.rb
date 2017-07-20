6427c69877724fdb8f7b520b8d3c28213410af5b
Suyash Agrawal <sagrawal@grepruby.com>
2017-07-20 20:12:21 +0530

    Added version

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -11,15 +11,18 @@ class PictureUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [200,200]
   end
 
+  version :small do 
+    process resize_to_fill: [50,50]
+  end
   # # Choose what kind of storage to use for this uploader:
   # storage :file
   # # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
