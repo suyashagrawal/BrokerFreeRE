443e7ac199f8c5bb34df4088439fc4159c614909
Suyash <sagrawal@grepruby.com>
2017-08-03 11:23:28 +0530

    Added Error Message for Country select

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require jquery.bxslider
@@ -19,4 +17,6 @@
 //= require property_images
 //= require country_state
 //= require twitter/bootstrap
+//= require rails-ujs
+//= require turbolinks
 //= require_tree .
--- db/migrate/20170802100555_add_email_to_sellers.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailToSellers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :sellers, :email, :string
-  end
-end
