0d80245f18bf5d24a5762dd5fe46c6cac762eaef
Suyash Agrawal <sagrawal@grepruby.com>
2017-08-08 12:42:28 +0530

    Added Image preview

--- app/assets/javascripts/image_upload_button.js
+++ app/assets/javascripts/image_upload_button.js
@@ -4,10 +4,24 @@ $(document).ready(function(){
   });
 
   $("#file_upload").on("change", function(){
-    var numFiles = $(this).get(0).files.length
-    if (numFiles > 1)
+    var numFiles = $('.image-preview img').length + $(this).get(0).files.length;
+
+    if (numFiles > 1) {
       $('.image-upload-div').html(numFiles + " files selected")
-    else
+    }
+    else {
       $('.image-upload-div').html(numFiles + " file selected")
+    }
+
+    loadFile($(this));
   });
+
+  var loadFile = function(event) {
+    var str = '';
+    $.each(event.get(0).files, function( index, value ) {
+      str += '<img src="' + URL.createObjectURL(value) + '"width="50" height="50" alt="Uploaded Image" title="Uploaded Image" />'
+    });
+
+    $('.image-preview').append(str);
+  };
 })
